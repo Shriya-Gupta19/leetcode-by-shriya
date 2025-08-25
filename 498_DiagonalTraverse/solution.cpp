bc3e444a11820a98487730f133c2d94535e8d63a
Shriya-Gupta19 <2k23.cscys2310697@gmail.com>
2025-08-25 22:09:46 +0530

    add solution for 498

---
+++ 498_DiagonalTraverse/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        int row=mat.size();
+        int col=mat[0].size();
+        vector<int>ans(row*col);
+        int i=0,j=0,k=0;
+        while(i<row*col){
+            ans[i]=mat[j][k];
+            if((j+k)%2==0){
+                if(k==col-1) j++;
+                else if(j==0) k++;
+                else j--,k++;
+            }
+            else{
+                if(j==row-1) k++;
+                else if(k==0) j++;
+                else j++,k--;
+            }
+            i++;
+        }
+        return ans;
+    }
+};
