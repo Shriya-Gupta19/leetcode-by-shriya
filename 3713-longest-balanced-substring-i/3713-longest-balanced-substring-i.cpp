cd32b72cb38940e99056ccec5e14acad170c1552
Shriya-Gupta19 <2k23.cscys2310697@gmail.com>
2026-02-12 22:29:44 +0530

    Time: 72 ms (91.77%), Space: 61.9 MB (6.1%) - LeetHub

--- 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
@@ -1,20 +1,27 @@
-int freq[26]={0};
 class Solution {
 public:
-    static int longestBalanced(string& s) {
-        int cnt=1, n=s.size();
-        for(int l=0; l<n; l++){
-            memset(freq, 0, sizeof(freq));
-            int uniq=0, maxF=0, cntMax=0;
-            for(int r=l; r<n; r++){
-                int f=++freq[s[r]-'a'];
-                uniq+=(f==1);
-                if (f>maxF){ maxF=f; cntMax=1; }
-                else if (f==maxF) cntMax++;
-                if (uniq==cntMax) 
-                    cnt=max(cnt, r-l+1);
+    int longestBalanced(string s) {
+        int n = s.length();
+        if (n == 0) return 0;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            vector<int> freq(128, 0);
+            int distinct = 0;
+            int maxFreq = 0;
+            for (int j = i; j < n; j++) {
+                freq[s[j]]++;
+
+                if (freq[s[j]] == 1) {
+                    distinct++;
+                }
+
+                maxFreq = max(maxFreq, freq[s[j]]);
+                if ((j - i + 1) == maxFreq * distinct) {
+                    ans = max(ans, j - i + 1);
+                }
             }
         }
-        return cnt;
+
+        return ans;
     }
 };
