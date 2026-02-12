8f04ce22d980499fac6c72ed6d4eb6f4794385b6
Shriya-Gupta19 <2k23.cscys2310697@gmail.com>
2026-02-12 23:59:02 +0530

    Time: 0 ms (100%), Space: 14.2 MB (96.87%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-    vector<bool>visit;
-    void solve(int i,vector<vector<int>>&rooms){
-        int n=rooms.size();
-        if(visit[i]==true) return;
-        visit[i]=true;
-        for(int r : rooms[i]){
-            solve(r,rooms);
+    vector<bool> visit;
+    int count = 0;
+
+    void solve(int i, vector<vector<int>>& rooms) {
+        if (visit[i]) return;
+
+        visit[i] = true;
+        count++;
+
+        for (int r : rooms[i]) {
+            solve(r, rooms);
         }
     }
+
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        int n=rooms.size();
+        int n = rooms.size();
         visit = vector<bool>(n, false);
-        solve(0,rooms);
-        for(int i=0;i<n;i++){
-            if(!visit[i]) return false;
-        }
-        return true;
+
+        solve(0, rooms);
+
+        return count == n;
     }
 };
