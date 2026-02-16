4a50b52fd5b26347691e72a666fe02b92f59b5bd
Shriya-Gupta19 <2k23.cscys2310697@gmail.com>
2026-02-16 21:09:16 +0530

    Time: 0 ms (100%), Space: 8.3 MB (46.07%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
-    uint32_t reverseBits(uint32_t n) { return rev(n, 32); }
-
-private:
-    uint32_t rev(uint32_t v, int len) {
-        if (len == 1) return v & 1u;
-
-        int half = len >> 1;
-        uint32_t mask = (1u << half) - 1u;
-        uint32_t lo = v & mask;
-        uint32_t hi = v >> half;
-
-        return (rev(lo, half) << half) | rev(hi, half);
+    int reverseBits(int n) {
+        int res=0;
+        for(int i=0;i<32;i++){
+            res<<=1;
+            res|=(n & 1);
+            n>>=1;
+        }
+        return res;
     }
 };
