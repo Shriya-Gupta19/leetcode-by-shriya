4f38c3d4f178b87961a1baeeef55fe49e5a63ddb
Shriya Gupta <2k23.cscys2310697@gmail.com>
2026-02-22 21:54:50 +0530

    Time: 0 ms (100%), Space: 8.1 MB (44.66%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.cpp
+++ 0868-binary-gap/0868-binary-gap.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     int binaryGap(int n) {
-        n >>= __builtin_ctz(n);
-        if (n == 1) return 0;
-        int maxGap = 0, gap = 0;
+        int maxd = 0;
+        int curr = 0;
+        bool found_first_one = false;
+        while (n > 0) {
+            int bit = n % 2;
 
-        while (n) {
-            if (n & 1) {
-                maxGap = max(maxGap, gap);
-                gap = 0;
-            } else
-                gap++;
-            n >>= 1;
+            if (bit == 1) {
+                if (found_first_one) {
+                    maxd = max(maxd, curr);
+                }
+                curr = 1;
+                found_first_one = true; 
+            } else {
+                if (found_first_one) curr++;
+            }
+            n /= 2;
         }
-
-        return maxGap + 1;
+        
+        return maxd;
     }
 };
