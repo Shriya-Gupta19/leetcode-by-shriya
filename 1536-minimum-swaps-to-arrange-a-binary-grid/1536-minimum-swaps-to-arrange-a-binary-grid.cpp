044368fa51692456a8e545da8a810bb7f08169ec
Shriya Gupta <2k23.cscys2310697@gmail.com>
2026-03-02 21:58:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1536-minimum-swaps-to-arrange-a-binary-grid/1536-minimum-swaps-to-arrange-a-binary-grid.cpp
+++ 1536-minimum-swaps-to-arrange-a-binary-grid/1536-minimum-swaps-to-arrange-a-binary-grid.cpp
@@ -2,28 +2,31 @@ class Solution {
 public:
     int minSwaps(vector<vector<int>>& grid) {
         int n = grid.size();
-        vector<int> zeros(n);
-
+        vector<int> trailingZeros(n);
         for (int i = 0; i < n; i++) {
             int count = 0;
-            for (int j = n - 1; j >= 0 && grid[i][j] == 0; j--) count++;
-            zeros[i] = count;
+            for (int j = n - 1; j >= 0; j--) {
+                if (grid[i][j] == 0)
+                    count++;
+                else
+                    break;
+            }
+            trailingZeros[i] = count;
         }
-
         int swaps = 0;
-
         for (int i = 0; i < n; i++) {
-            int needed = n - i - 1;
+            int required = n - 1 - i;
             int j = i;
-            while (j < n && zeros[j] < needed) j++;
-            if (j == n) return -1;
+            while (j < n && trailingZeros[j] < required)
+                j++;
+
+            if (j == n) return -1; 
             while (j > i) {
-                swap(zeros[j], zeros[j - 1]);
-                j--;
+                swap(trailingZeros[j], trailingZeros[j - 1]);
                 swaps++;
+                j--;
             }
         }
-
         return swaps;
     }
 };
