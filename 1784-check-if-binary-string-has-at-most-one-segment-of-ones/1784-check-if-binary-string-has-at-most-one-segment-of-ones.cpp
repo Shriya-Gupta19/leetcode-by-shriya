b89dd8bca2b04baff86f6ad4841c73986007f321
Shriya Gupta <2k23.cscys2310697@gmail.com>
2026-03-06 23:03:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
-    bool checkOnesSegment(string& s) {
-        const int n=s.size();
-        int i=s.find('0');
-        if (i==-1) return 1;
-        int i1=s.find_last_of('1');
-        return i>i1;
+    bool checkOnesSegment(string s) {
+        return s.find("01") == string::npos;
     }
 };
