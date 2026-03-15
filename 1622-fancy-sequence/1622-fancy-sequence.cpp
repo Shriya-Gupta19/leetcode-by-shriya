edd4cc3823841430db7d23f7d7161a07711be3ae
Shriya Gupta <2k23.cscys2310697@gmail.com>
2026-03-15 22:19:27 +0530

    Time: 68 ms (72.26%), Space: 192.5 MB (72.26%) - LeetHub

--- 1622-fancy-sequence/1622-fancy-sequence.cpp
+++ 1622-fancy-sequence/1622-fancy-sequence.cpp
@@ -1,40 +1,45 @@
 class Fancy {
 public:
-    const long long mod = 1e9 + 7;
+    static const long long MOD = 1e9 + 7;
     vector<long long> arr;
-    long long mul = 1;
-    long long add = 0;
+    long long mul, add;
 
-    long long modInverse(long long x) {
-        long long power = mod - 2;
+    Fancy() {
+        mul = 1;
+        add = 0;
+    }
+
+    long long modPow(long long a, long long b) {
         long long res = 1;
-        while(power) {
-            if(power & 1) res = (res * x) % mod;
-            x = (x * x) % mod;
-            power >>= 1;
+        a %= MOD;
+        while (b > 0) {
+            if (b & 1) res = (res * a) % MOD;
+            a = (a * a) % MOD;
+            b >>= 1;
         }
         return res;
     }
 
-    Fancy() {}
+    long long modInverse(long long x) {
+        return modPow(x, MOD - 2);
+    }
 
     void append(int val) {
-        long long x = ((val - add) % mod + mod) % mod;
-        x = (x * modInverse(mul)) % mod;
-        arr.push_back(x);
+        long long stored = ((val - add + MOD) % MOD * modInverse(mul)) % MOD;
+        arr.push_back(stored);
     }
 
     void addAll(int inc) {
-        add = (add + inc) % mod;
+        add = (add + inc) % MOD;
     }
 
     void multAll(int m) {
-        mul = (mul * m) % mod;
-        add = (add * m) % mod;
+        mul = (mul * m) % MOD;
+        add = (add * m) % MOD;
     }
 
     int getIndex(int idx) {
-        if(idx >= arr.size()) return -1;
-        return (arr[idx] * mul + add) % mod;
+        if (idx >= arr.size()) return -1;
+        return (arr[idx] * mul + add) % MOD;
     }
 };
