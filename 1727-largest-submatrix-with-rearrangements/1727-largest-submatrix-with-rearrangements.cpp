24b0d9351e30f11583000f8c7d6b2bbedc3dc345
Shriya Gupta <2k23.cscys2310697@gmail.com>
2026-03-17 22:32:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
@@ -1,20 +1,46 @@
 class Solution {
-public:
-    int largestSubmatrix(vector<vector<int>>& matrix) {
-        auto m = matrix.size(), n = matrix[0].size();
-        int res = 0;
-
-        for (int i = 1; i < m; i++)
-            for (int j = 0; j < n; j++)
-                if (matrix[i][j] == 1)
-                    matrix[i][j] += matrix[i - 1][j];
-
-        for (int i = 0; i < m; i++) {
-            sort(matrix[i].rbegin(), matrix[i].rend());
-            for (int j = 0; j < n; j++)
-                res = max(res, matrix[i][j] * (j + 1));
+public:  
+    static int largestSubmatrix(vector<vector<int>>& matrix) {
+        const int m=matrix.size(), n=matrix[0].size();
+        int area=count(matrix[0].begin(), matrix[0].end(), 1);
+        if (m==1) return area;
+        if (n==1){
+            for(int i=1; i<m; i++){
+                matrix[i][0]+=matrix[i][0]*matrix[i-1][0];
+                area=max(area, matrix[i][0]);
+            }
+            return area;
         }
-
-        return res;
+        
+        for(int i=1; i<m; i++){
+            for(int j=0;  j<n; j++){
+                matrix[i][j]+=matrix[i][j]*matrix[i-1][j];
+            }
+            const auto& row=matrix[i];
+            int minH=i+1, maxH=0;
+            for(int x: row){
+                minH=min(minH, x);
+                maxH=max(maxH, x);
+            }
+            vector<int> freq(maxH-minH+1, 0);
+            for(int x: row){
+                freq[x-minH]++;
+            }
+            int acc=0;
+            for(int x=maxH-minH; acc<n; x--){
+                if (freq[x]>0){
+                    acc+=freq[x];
+                    area=max(area, acc*(x+minH));
+                }
+            }  
+        }
+        return area;
     }
 };
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
