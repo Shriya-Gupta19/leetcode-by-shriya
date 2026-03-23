4b89f2d00d46ff50916328439cba04643e31ccfd
Shriya Gupta <2k23.cscys2310697@gmail.com>
2026-03-23 12:45:09 +0530

    Time: 0 ms (100%), Space: 16.4 MB (5.52%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    vector<bool> visit;
-    int count = 0;
-
-    void solve(int i, vector<vector<int>>& rooms) {
-        if (visit[i]) return;
-
-        visit[i] = true;
-        count++;
-
-        for (int r : rooms[i]) {
-            solve(r, rooms);
-        }
-    }
-
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        int n = rooms.size();
-        visit = vector<bool>(n, false);
-
-        solve(0, rooms);
-
-        return count == n;
+        unordered_map<int, vector<int>> mp;
+        for (int i = 0; i<rooms.size(); i++) {
+            for (auto &key : rooms[i]) {
+                mp[i].push_back(key);
+            }
+        }
+        vector<bool> visited(rooms.size(), false);
+        queue<int> q;
+        q.push(0);
+        visited[0] = true;
+        while(!q.empty()) {
+            int u = q.front();
+            q.pop();
+            for (int &v : mp[u]) {
+                if (!visited[v]) {
+                    q.push(v);
+                    visited[v] = true;
+                }
+            }
+        }
+        for (bool b : visited) {
+            if (!b) return false;
+        }
+        return true;
     }
 };
