53e7be7781588eff59c031d77f3c8be2684043cb
Shriya Gupta <2k23.cscys2310697@gmail.com>
2026-04-01 09:28:53 +0530

    Time: 32 ms (99.8%), Space: 198 MB (38.32%) - LeetHub

--- 2751-robot-collisions/2751-robot-collisions.cpp
+++ 2751-robot-collisions/2751-robot-collisions.cpp
@@ -1,50 +1,39 @@
 class Solution {
 public:
-    vector<int> survivedRobotsHealths(vector<int>& pos, vector<int>& h, string d) {
+    using int2=pair<int, int>;// (position, idx)
+    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string& directions) {
+        const int n=positions.size();
+        vector<int2> robot(n);
+        for(int i=0; i<n; i++)// 0-indexed is fine
+            robot[i]={positions[i], i};
+        sort(robot.begin(), robot.end(), greater<>());
 
-        int n = pos.size();
-        vector<int> order(n);
-        iota(order.begin(), order.end(), 0);
-
-        sort(order.begin(), order.end(),
-             [&](int a,int b){ return pos[a] < pos[b]; });
-
-        vector<bool> alive(n,true);
-        vector<int> st;
-
-        for(int idx:order){
-
-            if(d[idx]=='R') st.push_back(idx);
-
+        vector<int> stack;
+        for(auto& [pos, i]: robot){
+            if (directions[i]=='L') stack.push_back(i);
             else{
-                while(!st.empty()){
-
-                    int top = st.back();
-
-                    if(h[top] < h[idx]){
-                        alive[top]=false;
-                        st.pop_back();
-                        h[idx]--;
-                    }
-                    else if(h[top] > h[idx]){
-                        alive[idx]=false;
-                        h[top]--;
-                        break;
-                    }
-                    else{
-                        alive[top]=false;
-                        alive[idx]=false;
-                        st.pop_back();
-                        break;
-                    }
+                while(!stack.empty() && healths[i]>0){
+                    int j=stack.back();
+                    int x=healths[j]-healths[i];
+                    if (x>0) healths[j]--, healths[i]=0;
+                    else if (x<0) healths[j]=0, healths[i]--, stack.pop_back();
+                    else healths[i]=healths[j]=0, stack.pop_back();
                 }
             }
         }
-
-        vector<int> res;
-        for(int i=0;i<n;i++)
-            if(alive[i]) res.push_back(h[i]);
-
-        return res;
+        vector<int> ans;
+        for(int x: healths)
+            if (x>0) ans.push_back(x);
+        return ans;
     }
 };
+
+
+
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
