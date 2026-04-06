bb8db48b3dafc8a7759085ef2bbdda24ae322861
Shriya Gupta <2k23.cscys2310697@gmail.com>
2026-04-06 16:41:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
@@ -1,36 +1,50 @@
-#include <memory_resource>
-pmr::unsynchronized_pool_resource pool;
 class Solution {
 public:
+
+    // custom hash for pair<int,int>
+    struct pair_hash {
+        size_t operator()(const pair<int,int>& p) const {
+            return ((long long)p.first << 32) ^ (p.second);
+        }
+    };
+
     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-        const unsigned lb=30000;
-        pmr::unordered_set<unsigned> obSet(&pool);
-        obSet.reserve(obstacles.size());
-        for(auto& ob: obstacles){
-            const unsigned x=ob[0]+lb, y=ob[1]+lb;
-            obSet.insert((x<<16)+y);
+        unordered_set<pair<int,int>, pair_hash> st;
+
+        for (int i=0;i<obstacles.size();i++) {
+            st.insert({obstacles[i][0], obstacles[i][1]});
         }
 
-        const int dir[4][2]={{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
-        int x=0, y=0, dx=0, dy=1, face=0, maxD2=0;
-        for(int c: commands){
-            switch(c){
-                case -2: face=(face+1)&3; dx=dir[face][0]; dy=dir[face][1]; break;
-                case -1: face=(face+3)&3; dx=dir[face][0]; dy=dir[face][1]; break;
-                default:
-                //    cout<<"\nc="<<c<<" face="<<face<<":";
-                    for(int i=0; i<c; i++){
-                        x+=dx, y+=dy;
-                        if (obSet.count(((x+lb)<<16)+y+lb)) {
-                            x-=dx;  // previous move
-                            y-=dy;
-                            break;
-                        }
-                    //    cout<<"("<<x<<","<<y<<"),";
-                        maxD2=max(maxD2, x*x+y*y);
-                    }
+        vector<pair<int,int>> dir = {
+            {0,1}, {1,0}, {0,-1}, {-1,0}
+        };
+
+        int d = 0; // North
+        int x = 0, y = 0;
+        int ans = 0;
+
+        for (int i=0;i<commands.size();i++) {
+            if (commands[i] == -1) {
+                d = (d + 1) % 4;
+            } 
+            else if (commands[i] == -2) {
+                d = (d + 3) % 4;
+            } 
+            else {
+                for (int j = 0; j < commands[i]; j++) {
+                    int nx = x + dir[d].first;
+                    int ny = y + dir[d].second;
+
+                    if (st.count({nx, ny})) break;
+
+                    x = nx;
+                    y = ny;
+
+                    ans = max(ans, x*x + y*y);
+                }
             }
         }
-        return maxD2;
+
+        return ans;
     }
 };
