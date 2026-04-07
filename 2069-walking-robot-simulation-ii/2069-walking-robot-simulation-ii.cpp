e0017fc44c80a39d9ff77ea5a5e53f35c39f6f94
Shriya Gupta <2k23.cscys2310697@gmail.com>
2026-04-07 20:04:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.cpp
+++ 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.cpp
@@ -1,77 +1,65 @@
 class Robot {
-    int x; 
-    int y;
-    int width; 
-    int height; 
-    string dir; 
 public:
+    int w, h;
+    int x = 0, y = 0;
+    int dir = 0; // 0=East,1=North,2=West,3=South
+    int per;
+
     Robot(int width, int height) {
-        x = 0, y = 0, dir = "East";
-        this->width = width, this->height = height;
+        w = width;
+        h = height;
+        per = 2 * (w + h) - 4;
     }
     
     void step(int num) {
-        num %= (2 * (width - 1) + 2 * (height - 1));
-        if (num == 0) {
-            num = (2 * (width - 1) + 2 * (height - 1));
+        if (per == 0) return;
+
+        num %= per;
+
+        // special case
+        if (num == 0 && (x != 0 || y != 0)) return;
+        if (num == 0 && x == 0 && y == 0) {
+            dir = 3; // South
+            return;
         }
 
-        while(num > 0) {
-            int nx = x, ny = y; 
-            
-            if(dir == "East") {
-                int maxX = min(x + num, width - 1); 
-                int rem = num - (maxX - x); 
-                num = rem; 
-                if(rem == 0) {
-                    x = maxX, y = ny; 
-                } else {
-                    x = maxX; 
-                    dir = "North"; 
-                }
-            }
-            else if(dir == "West") {
-                int minX = max(x - num, 0); 
-                int rem = num - (x - minX); 
-                num = rem; 
-                if(rem == 0) {
-                    x = minX, y = ny; 
-                } else {
-                    x = minX; 
-                    dir = "South"; 
-                }
-            }
-            else if(dir == "North") {
-                int maxY = min(y + num, height - 1); 
-                int rem = num - (maxY - y); 
-                num = rem; 
-                if(rem == 0) {
-                    x = nx, y = maxY; 
-                } else {
-                    y = maxY; 
-                    dir = "West"; 
-                }
-            }
-            else if(dir == "South") {
-                int minY = max(y - num, 0); 
-                int rem = num - (y - minY); 
-                num = rem; 
-                if(rem == 0) {
-                    x = nx, y = minY; 
-                } else {
-                    y = minY; 
-                    dir = "East"; 
-                }
+        while (num > 0) {
+            if (dir == 0) { // East
+                int move = min(num, w - 1 - x);
+                x += move;
+                num -= move;
+                if (num > 0) dir = 1;
+            } 
+            else if (dir == 1) { // North
+                int move = min(num, h - 1 - y);
+                y += move;
+                num -= move;
+                if (num > 0) dir = 2;
+            } 
+            else if (dir == 2) { // West
+                int move = min(num, x);
+                x -= move;
+                num -= move;
+                if (num > 0) dir = 3;
+            } 
+            else { // South
+                int move = min(num, y);
+                y -= move;
+                num -= move;
+                if (num > 0) dir = 0;
             }
         }
     }
     
     vector<int> getPos() {
-        return {x, y}; 
+        return {x, y};
     }
     
     string getDir() {
-        return dir; 
+        if (dir == 0) return "East";
+        if (dir == 1) return "North";
+        if (dir == 2) return "West";
+        return "South";
     }
 };
 
