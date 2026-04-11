32b4fec50ca99dfd255e2e3f5743a147f0eff7bb
Shriya Gupta <2k23.cscys2310697@gmail.com>
2026-04-11 11:13:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cpp
+++ 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     int minimumDistance(vector<int>& nums) {
-        vector<array<int, 2>> V(100001, {-1, -1});
-
-        int Ans = 1e9;
-
-        for (int i = 0; i < nums.size(); i++) {
-            int x = nums[i];
-
-            if (V[x][0] == -1) {
-                V[x][0] = i;
-            } 
-            else if (V[x][1] == -1) {
-                V[x][1] = i;
-            } 
-            else {
-                // Directly using derived formula
-                Ans = min(Ans, 2 * (i - V[x][0]));
-
-                // shift window
-                V[x][0] = V[x][1];
-                V[x][1] = i;
+        unordered_map<int,vector<int>> mp;
+        int n = nums.size();
+        
+        for(int i = 0; i < n; i++){
+            mp[nums[i]].push_back(i);
+        }
+        int mini = INT_MAX;
+        for(auto &it : mp){
+            vector<int> &temp = it.second;
+            int m = temp.size();
+            if(m >= 3){
+                for(int i = 0; i < m - 2; i++){
+                    int a = temp[i];
+                    int b = temp[i+1];
+                    int c = temp[i+2];
+                    
+                    int diff = 2 * (max({a,b,c}) - min({a,b,c}));
+                    mini = min(mini, diff);
+                }
             }
         }
-
-        return (Ans == 1e9) ? -1 : Ans;
+        
+        return mini == INT_MAX ? -1 : mini;
     }
 };
