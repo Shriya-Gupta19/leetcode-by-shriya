cfe3939aa43e284ae5933f7185187345d9d47f2c
Shriya Gupta <2k23.cscys2310697@gmail.com>
2026-04-19 21:00:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        vector<string>words;
-        string c="";
-        for(int i=0;i<s.size();i++){
-            if(s[i]!= ' '){
-                c+=s[i];
-            }
-            else if(!c.empty()){
-                words.push_back(c);
-                c="";
-            }
-        }
-        if(!c.empty()){
-            words.push_back(c);
-        }
         string res="";
-        reverse(words.begin(),words.end());
-        for(int i=0;i<words.size();i++){
-            res+=words[i];
-            if (i < words.size() - 1) {
-                res += " ";
+        int n=s.size();
+        int i=n-1;
+        while(i>=0){
+            while (i >= 0 && s[i] == ' ') {
+                i--;
             }
+            if(i<0) break;
+            int end=i;
+            while (i >= 0 && s[i] != ' ') {
+                i--;
+            }
+            string word=s.substr(i+1,end-i);
+            if(!res.empty()){
+                res+=' ';
+            }
+            res+=word;
         }
-
         return res;
     }
 };
