9a3b0a0f57be9b308ddc52e4301106cedfd2945d
Shriya Gupta <2k23.cscys2310697@gmail.com>
2026-04-28 10:34:53 +0530

    Time: 41 ms (51.85%), Space: 90 MB (53.91%) - LeetHub

--- 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
-        int n = grid.size(), m = grid[0].size();
-        int N = n * m;
-        int freq[10001] = {0};
-        int mn = grid[0][0], mx = mn;
+        vector<int> arr;
 
-        for (const auto& row : grid) {
-            for (int val : row) {
-                if ((val - grid[0][0]) % x != 0) return -1;
-                freq[val]++;
-                mn = min(mn, val);
-                mx = max(mx, val);
-            }
+        // flatten grid
+        for(auto &row : grid){
+            for(int v : row)
+                arr.push_back(v);
         }
 
-        int target = (N + 1) / 2;
-        int acc = 0, median = mn;
-
-        for (int i = mn; i <= mx; i += x) {
-            acc += freq[i];
-            if (acc >= target) {
-                median = i;
-                break;
-            }
+        // check possible
+        int base = arr[0];
+        for(int v : arr){
+            if(abs(v - base) % x != 0)
+                return -1;
         }
 
+        // sort
+        sort(arr.begin(), arr.end());
+
+        // median
+        int median = arr[arr.size()/2];
+
+        // count operations
         int ops = 0;
-        for (int i = mn; i <= mx; i += x)
-            ops += abs(i - median) / x * freq[i];
+        for(int v : arr){
+            ops += abs(v - median) / x;
+        }
 
         return ops;
     }
