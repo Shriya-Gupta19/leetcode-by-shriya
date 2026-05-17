ed42bbabdd371d425eb11ad71d2d068941a6e09f
Shriya Gupta <2k23.cscys2310697@gmail.com>
2026-05-17 17:12:59 +0530

    Time: 0 ms (100%), Space: 34.9 MB (95.79%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,29 +1,24 @@
+constexpr int N=5e4;
+int q[N], front=0, back=0;
+bool vis[N];
 class Solution {
 public:
-    bool canReach(vector<int>& arr, int start) {
-        int n = arr.size();
-
-        vector<bool> visited(n, false);
-        queue<int> q;
-
-        q.push(start);
-
-        while (!q.empty()) {
-            int i = q.front();
-            q.pop();
-
-            if (i < 0 || i >= n || visited[i])
-                continue;
-
-            if (arr[i] == 0)
-                return true;
-
-            visited[i] = true;
-
-            q.push(i + arr[i]);
-            q.push(i - arr[i]);
+    static bool canReach(vector<int>& arr, int start) {
+        const int n=arr.size();
+        front=back=0;// reset q
+        q[back++]=start;
+        vis[start]=1;
+        while(front<back){
+            const int i=q[front++];
+            const int x=arr[i], l=i-x, r=i+x;
+            if (x==0){
+                memset(vis, 0, n);// reset for next testcase
+                return 1;
+            }
+            if (l>=0 && !vis[l]) q[back++]=l, vis[l]=1;
+            if (r<n  && !vis[r]) q[back++]=r, vis[r]=1;
         }
-
-        return false;
+        memset(vis, 0, n);// reset vis
+        return 0;
     }
 };
