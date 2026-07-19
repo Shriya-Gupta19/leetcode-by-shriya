8c0c1558e43acbc3b697cdfb133a7297475b5b89
Shriya Gupta <2k23.cscys2310697@gmail.com>
2026-07-19 13:20:24 +0530

    Time: 17 ms (29.86%), Space: 12.6 MB (36.74%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,7 +4,7 @@ public:
         int low=0,high=0;
         int res=0;
         int k;
-        unordered_map<char,int>mp;
+        unordered_map<int,int>mp;
         for(int high=0;high<s.size();high++){
             mp[s[high]]++;
             k=high-low+1;
