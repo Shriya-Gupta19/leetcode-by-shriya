df27b8b0c4c1ad0e4aac644fd399ef6f27c48d31
Shriya Gupta <2k23.cscys2310697@gmail.com>
2026-07-29 21:26:42 +0530

    Time: 34 ms (33.33%), Space: 88.1 MB (39.29%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
-    int m, n;
-    int memo[1000][1000];
-    int maximumScore(vector<int>& nums, vector<int>& muls) {
-        n = nums.size();
-        m = muls.size();
-        memset(memo, -1, sizeof(memo));
-        return dp(0, 0, nums, muls);
+    vector<vector<int>> dp;
+    
+    int solve(int i, int n, int j, vector<int> &nums, vector<int> &M){
+        
+        if (j == M.size()) return 0;
+        if (dp[i][j] != INT_MIN) return dp[i][j];
+        
+        // Left Side
+        int left = solve(i + 1, n, j + 1, nums, M) + (nums[i] * M[j]);
+        
+        // Right Side
+        int right = solve(i, n, j + 1, nums, M) + (nums[(n - 1) - (j - i)] * M[j]);
+        
+        return dp[i][j] = max(left, right);
     }
-    int dp(int l, int i, vector<int>& nums, vector<int>& muls) {
-        if (i == m) return 0; 
-        if (memo[l][i] != -1) return memo[l][i];
-        int pickLeft = dp(l+1, i+1, nums, muls) + nums[l] * muls[i]; 
-        int pickRight = dp(l, i+1, nums, muls) + nums[n-(i-l)-1] * muls[i]; 
-        return memo[l][i] = max(pickLeft, pickRight);
+    
+    int maximumScore(vector<int>& nums, vector<int>& M) {   
+        int n = nums.size(), m = M.size();
+        dp.resize(m + 1, vector<int>(m + 1, INT_MIN));
+        return solve(0, n, 0, nums, M);
     }
 };
