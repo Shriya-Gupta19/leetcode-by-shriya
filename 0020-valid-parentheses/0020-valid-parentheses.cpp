de16ed44e543bd565cac1baf977f8404ea1386b0
Shriya Gupta <2k23.cscys2310697@gmail.com>
2026-08-13 21:54:05 +0530

    Time: 0 ms (100%), Space: 8.8 MB (89.72%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='('||s[i]=='['||s[i]=='{'){
+                st.push(s[i]);
+                continue;
+            }
+            if(st.empty()){
+                return false;
+            }
+            if(s[i]==')' and st.top()=='(' ) {
+                st.pop();
+            }
+            else if(s[i]==']' and st.top()=='[' ) {
+                st.pop();
+            }  
+            else if(s[i]=='}' and st.top()=='{' ) {
+                st.pop();
+            }
+            else return false; 
+        }
+        return st.empty();    
+    }
+};
