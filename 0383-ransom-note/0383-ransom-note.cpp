4be89d0385592f35ad64956600ea27d3a2024764
Shriya Gupta <2k23.cscys2310697@gmail.com>
2026-09-11 12:08:48 +0530

    Time: 3 ms (52.15%), Space: 12 MB (29.34%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int>have;
+        for(int i=0;i<magazine.size();i++){
+            have[magazine[i]]++;
+        }
+        unordered_map<char,int>need;
+        for(int i=0;i<ransomNote.size();i++){
+            need[ransomNote[i]]++;
+        }
+        for(auto i:need){
+            char c=i.first;
+            int fneed=i.second;
+            int fhave=have[c];
+            if(fhave<fneed) return false;
+        }
+        return true;
+    }
+};
